a9d345e4a0bfa45013e0c1c23620c22d28f38396
Sophroa <cn_qkdwtp@outlook.com>
2025-11-03 11:13:31 +0800

    CSS的内容更新

---
+++ JS的20个例子/数组/创建数组.js
@@ -0,0 +1,7 @@
+let arr1 = [1,2,'qq',true];
+
+for(let i=0;i<arr1.length;i++) console.log(arr1[i]);
+
+let arr2 = new  Array(1,2,'qq',true);
+
+for(let i=0;i<arr2.length;i++)  document.write(arr2[i]);
